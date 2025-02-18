2094c81b464718f43ff142ba1da78e77533b957e
Baggiio <gustavo.gbaggio@gmail.com>
2025-02-18 13:43:06 -0300

    adjust batch size and new model

--- chatbot.py
+++ chatbot.py
@@ -114,7 +114,7 @@ class ChatBot:
         model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
         # fitting and saving the model
-        hist = model.fit(np.array(train_x), np.array(train_y), epochs=2000, batch_size=5, verbose=1)
+        hist = model.fit(np.array(train_x), np.array(train_y), epochs=5000, batch_size=16, verbose=1)
         model.save('chatbot_model.h5', hist)
 
         self.model = model
